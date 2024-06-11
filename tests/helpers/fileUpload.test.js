ddf4ba3f5ff9c3cea4781586159392fad10e661e
Nahuel Garcia <nhlgrc@hotmail.com>
2024-06-11 17:36:11 -0300

    234234

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,10 +1,18 @@
+import { v2 as cloudinary } from 'cloudinary';
 import { fileUpload } from "../../src/helpers/fileUpload";
+
+cloudinary.config({
+    cloud_name: 'dzxkeidxx',
+    api_key: '591147753968845',
+    api_secret: 'vz1CjGgNp6G3vKtfrBUzMiuUCtY',
+    secure: true
+});
 
 describe('Pruebas en fileUpload', () => {
 
     test('debe subir el archivo correctamente a cloudinary', async () => {
         
-        const imageURL = '    https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png';
+        const imageURL = 'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png';
 
         const resp = await fetch( imageURL );
 
@@ -15,6 +23,11 @@ describe('Pruebas en fileUpload', () => {
         const url = await fileUpload( file );
 
         expect( typeof url ).toBe("string");
+
+        const segments = url.split('/');
+        const imageId = segments[ segments.length - 1].replace('.png','');
+
+        await cloudinary.api.delete_resources([ imageId ]);
     });
 
     test('debe retornar null', async () => {
