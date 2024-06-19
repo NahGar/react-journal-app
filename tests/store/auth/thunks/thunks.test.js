bd415b3a8f03f8685d268b48ca3923d8274eceed
Nahuel Garcia <nhlgrc@hotmail.com>
2024-06-19 15:44:18 -0300

    12312

--- tests/store/auth/thunks/thunks.test.js
+++ tests/store/auth/thunks/thunks.test.js
@@ -1,10 +1,45 @@
-import { checkingAuthentication } from "../../../../src/store/auth/thunks";
+import { signInWidthGoogle } from "../../../../src/firebase/providers";
+import { checkingCredentials, login, logout } from "../../../../src/store/auth";
+import { checkingAuthentication, startGoogleSignIn } from "../../../../src/store/auth/thunks";
+import { demoUser } from "../../../fixtures/authFixtures";
 
+//mock completo a todos los providers
 jest.mock("../../../../src/firebase/providers");
 
 describe('Pruebas en AuthThunks', () => { 
     
-    test('debe invocar checkingAuthentication', () => { 
-        checkingAuthentication();
+    const dispatch = jest.fn();
+
+    beforeEach( () => jest.clearAllMocks() );
+
+    test('debe invocar checkingCredentials', async () => { 
+        
+        //()=llamdo de la función ()=parámetros para la función que devuelve
+        await checkingAuthentication()( dispatch );
+
+        expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() );
     });
+
+    test('startGoogleSignIn debe invocar checkingCredentials y login - Exito', async () => { 
+        
+        const loginData = { ok: true, ...demoUser };
+        //es un mock por el mock completo creado
+        await signInWidthGoogle.mockResolvedValue( loginData );
+        await startGoogleSignIn()( dispatch );
+
+        expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() );
+        expect( dispatch ).toHaveBeenCalledWith( login( loginData ) );
+    });
+
+    test('startGoogleSignIn debe invocar checkingCredentials y logout - Error', async () => { 
+        
+        const logoutData = { ok: false, errorMessage: 'Error xxx en Google' };
+        //es un mock por el mock completo creado
+        await signInWidthGoogle.mockResolvedValue( logoutData );
+        await startGoogleSignIn()( dispatch );
+
+        expect( dispatch ).toHaveBeenCalledWith( checkingCredentials() );
+        expect( dispatch ).toHaveBeenCalledWith( logout( logoutData.errorMessage ) );
+    });
+
 });
