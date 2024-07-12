1c19673f5fc0d51e9fad904b7ca341f199fe1194
Nahuel Garcia <nhlgrc@hotmail.com>
2024-07-12 13:35:00 -0300

    123123

--- tests/journal/thunks.test.js
+++ tests/journal/thunks.test.js
@@ -1,6 +1,7 @@
 import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../src/firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote, startNewNote } from "../../src/store/journal";
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes, startLoadingNotes, startNewNote } from "../../src/store/journal";
+import { loadNotes } from "../../src/helpers/loadNotes";
 
 describe('Pruebas en Journal Thunks', () => {
 
@@ -11,7 +12,7 @@ describe('Pruebas en Journal Thunks', () => {
 
     test('startNewNote debe crear una nueva nota en blanco', async() => {
 
-        getState.mockReturnValue({ auth: { uid: 'TEST-UID' } })
+        getState.mockReturnValue({ auth: { uid: 'TEST-UID' } });
         await startNewNote()( dispatch, getState );
 
         expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
@@ -29,12 +30,29 @@ describe('Pruebas en Journal Thunks', () => {
         }));
 
         //borrar de firebase
-        const collectionRef = collection( FirebaseDB, `TEST-UID/journal/notes`);
-        const docs = await getDocs( collectionRef );
+        //const collectionRef = collection( FirebaseDB, `TEST-UID/journal/notes`);
+        //const docs = await getDocs( collectionRef );
 
-        const deletePromises = [];
-        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ));
+        //const deletePromises = [];
+        //docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ));
 
-        await Promise.all( deletePromises );
+        //await Promise.all( deletePromises );
     });
+
+    test('startLoadingNotes debe cargar notas', async() => {
+
+        const uid = 'TEST-UID';
+        getState.mockReturnValue({ auth: { uid: uid } });
+
+        await startLoadingNotes()( dispatch, getState );
+
+        //await waitFor( () => expect(dispatch).toHaveBeenCalledWith( loadNotes({ uid: 'TEST-UID'}) ))
+        //expect( dispatch ).toHaveBeenCalledWith( await loadNotes( uid ) );
+        //expect( dispatch ).toHaveBeenCalled( await loadNotes( uid ));
+        //expect( loadNotes ).toHaveBeenCalled();
+        //expect( dispatch ).toHaveBeenCalled( loadNotes( uid ) );
+        
+    });
+
+
 });
